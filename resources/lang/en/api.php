5bc52ac3ed650bfde2bb821e5926c1fe0c89e91a
maniyadv <manishady@gmail.com>
2019-02-03 23:36:37 +0000

    api lang file added

---
+++ resources/lang/en/api.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maniyadav
+ * Date: 2/3/19
+ * Time: 11:32 PM
+ */
+
+
+return [
+    "no-valid-record-found" => "No Valid Record found based on input data",
+];
