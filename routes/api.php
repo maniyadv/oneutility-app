02b441013e1c648d9d8e48a9a489dc8e03228e00
maniyadav <manishady@gmail.com>
2019-02-03 12:39:44 +0000

    Very first commit with Passport Auth and Swagger installation postman collection sample and swagger sample added

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::group(['prefix' => 'v1', 'namespace' => 'API'], function () {
+
+    // public routes
+    Route::post('/login', 'AuthController@login')->name('login.api');
+    Route::post('/register', 'AuthController@register')->name('register.api');
+
+    // private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'AuthController@logout')->name('logout');
+    });
+
+});
+
